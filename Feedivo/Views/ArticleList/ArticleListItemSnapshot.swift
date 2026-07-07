dd5d66698243c04266cbdcdb8d2d2c1839e4af18
Martin Felder <martin.felder@outlook.com>
2026-07-07 06:39:04 +0200

    Remove dead Article-based initializer from ArticleListItemSnapshot

--- Feedivo/Views/ArticleList/ArticleListItemSnapshot.swift
+++ Feedivo/Views/ArticleList/ArticleListItemSnapshot.swift
@@ -1,5 +1,4 @@
 import Foundation
-import SwiftData
 
 struct ArticleListItemSnapshot: Equatable, Identifiable {
     let id: String
@@ -15,22 +14,6 @@ struct ArticleListItemSnapshot: Equatable, Identifiable {
     let imageURL: String?
     let offlineState: ArticleOfflineState
     let hasOriginalURL: Bool
-
-    init(article: Article, feedTitle: String?) {
-        self.id = String(describing: article.persistentModelID)
-        self.title = article.title
-        self.summary = article.summary
-        self.publishedAt = article.publishedAt
-        self.feedID = article.feedID
-        self.feedTitle = feedTitle
-        self.isRead = article.isRead
-        self.isStarred = article.isStarred
-        self.isArchived = article.isArchived
-        self.isHidden = article.isHidden
-        self.imageURL = article.imageURL
-        self.offlineState = article.offlineState
-        self.hasOriginalURL = ArticleOriginalURLResolver.hasUsableWebLink(article.link)
-    }
 
     init(sqliteSnapshot: ArticleListSnapshot) {
         self.id = sqliteSnapshot.id
